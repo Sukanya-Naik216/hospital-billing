dde82c029caf92506b01d5a5cb908b93c7cdeb24
sukanya <naiksukanya021@gmail.com>
2026-04-16 13:49:52 +0530

    fix: use local DB and server for development

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://hospital-billing-api.onrender.com/api' });
+const API = axios.create({ baseURL: 'http://localhost:5000/api' });
 
 API.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
